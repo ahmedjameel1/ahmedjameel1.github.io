9b4cbadc1ec8f023b724bc256b2556a94fe63d75
AhmedJameel <roarme247@gmail.com>
2023-06-05 15:35:21 +0200

    sec commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var maps = document.querySelectorAll(".about");
+var mapBox = document.querySelector(".about-all");
+
+maps.forEach(function(div) {
+    var arrow = div.querySelector(".arrow");
+    var info = div.querySelector('div.hidden'); // Update selector to target both <p> and <div> elements with class 'hidden'
+
+    div.addEventListener('click', function() {
+        arrow.classList.toggle("opened");
+        info.classList.toggle("hidden");
+    });
+
+    info.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+});
+
+
+
+
+
